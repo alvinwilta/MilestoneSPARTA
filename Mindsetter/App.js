8a30dcf81737510fcac0ade061ac54d6fd262361
458-nadiraF <nadira.fwzyya@gmail.com>
2020-08-07 17:23:02 +0700

    adding Profile Screen

--- Mindsetter/App.js
+++ Mindsetter/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native';
 
-import Login from './Screens/Login'
-
-//import Home from './Screens/HomeScreen'
+import BackgroundHeader from './components/BackgroundHeader';
+import BottomTab from './components/BottomTab';
+import { ScrollView } from 'react-native-gesture-handler';
+import ProfileScreen from './Screens/ProfileScreen';
 /*
+import Login from './Screens/Login'
+import LetterOfCommitment from './Screens/LetterOfCommitment'
+import Home from './Screens/HomeScreen'
+
 
 export default function() {
   return (
     <Home />
   );
 }
-*/
+
 export default class App extends Component<{}> {
   render() {
     return (
@@ -20,17 +25,42 @@ export default class App extends Component<{}> {
           backgroundColor="#1c313a"
           barStyle="light-content"
         />
-        <Login/>
+        <LetterOfCommitment/>
       </View>
     );
   }
 }
+*/
+const App = () => {
+  return(
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container} >
+        <BackgroundHeader style={styles.bg} />
+        <ScrollView style={styles.scrollView}>
+          <ProfileScreen />
+        </ScrollView>
+        <BottomTab />
+
+      </View>
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2C94A6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  scrollView: {
+    flex: 1,
+  },
+  bg: {
+    position: 'absolute',
+    width: Dimensions.get('window').width
   },
 });
   
+export default App;
